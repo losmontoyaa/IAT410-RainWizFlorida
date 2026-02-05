845fd57431f683f47cc1e82d385225bae040c0e2
ElDanny789 <142043752+ElDanny789@users.noreply.github.com>
2026-02-04 23:34:48 -0800

    updated Assets folder

---
+++ Assets/Assets/Scripts/RainWizzardScript.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+//sing UnityEngine.InputSystem;
+
+
+public class RainWizzardScript : MonoBehaviour
+{
+
+    [SerializeField] private float speed = 5f;
+
+    [SerializeField] private float gravity = -9.81f;
+    [SerializeField] private float groundedStick = -2f;
+    private GameObject telephonePole;
+    private int state = 0;
+
+
+
+    private CharacterController characterController;
+    private float yVelocity;
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+
+        Vector3 move = new Vector3(x, 0f, z).normalized * speed;
+        
+
+        if (characterController.isGrounded && yVelocity < 0f)
+            yVelocity = groundedStick;
+
+        yVelocity += gravity * Time.deltaTime;
+
+        // Combine XZ + Y
+        Vector3 velocity = new Vector3(move.x, yVelocity, move.z);
+        characterController.Move(velocity * Time.deltaTime);
+    }
+
+    public void ResetVerticalVelocity()
+    {
+        yVelocity = 0f;
+    }
+
+
+
+
+}
