4643abfb9b842d8b9f0f40446d9d23e0b598cd74
stackofchairs <78242471+stackofchairs@users.noreply.github.com>
2026-03-02 10:46:05 -0800

    Black outline shaders added
    still need to refine

---
+++ Assets/Shaders/Outlines.hlsl
@@ -0,0 +1,40 @@
+struct ScharrOperators {
+float3x3 x;
+float3x3 y;
+};
+
+ScharrOperators GetEdgeDetectionKernels()
+{
+    ScharrOperators kernels;
+    kernels.x = float3x3(-3, -10, -3, 0, 0, 0, 3, 10, 3);
+    kernels.y = float3x3(-3, 0, 3, -10, 0, 10, -3, 0, 3);
+    return kernels;
+
+}
+
+void DepthBasedOutlines_float(float2 screenUV, float2 px, out float outlines)
+{
+    outlines = 0;
+    #if defined(UNITY_DECLARE_DEPTH_TEXTURE_INCLUDED)
+    ScharrOperators kernels = GetEdgeDetectionKernels();
+    floatgx = 0; 
+    floatgy = 0;
+    for (int i = -1; i <= 1; i++)
+    {
+        for (int j = -1; j <= 1; j++)
+        {
+            if (i == 0 && j == 0)
+                continue;
+            float2 offset = float2(i, j) * px;
+            float d = SampleSceneDepth(screenUV + offset);
+            gx += d * kernels.x[i + 1][j + 1];
+            gy += d * kernels.y[i + 1][j + 1];
+
+        }
+
+    }
+    float g = sqrt(gx * gx + gy * gy);
+    outlines = step(.02, g);
+
+    #endif 
+}
